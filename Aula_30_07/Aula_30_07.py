440362926f30e15ab6642719d244aecf88bfc2da
Thiago Gomes <@thiagogomes20061981@gmail.com>
2026-07-30 20:23:00 -0300

    Exemplos de python na aula dia 30/07

---
+++ Aula_30_07/Aula_30_07.py
@@ -0,0 +1,9 @@
+def criar_lista():
+    # a lista [1,2,3] vai para o Heap
+    numeros = [1,2,3]
+    return numeros
+
+    resultado = criar_lista()
+    # A função acabou, mas a lista
+    # ainda existe no Heap!
+    print(resultado) # [1,2,3]
